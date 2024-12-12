647b311330621b1ef2d3bdcbd31b51245c4a6949
Jose F. Romaniello <jose@okta.com>
2024-12-12 12:47:01 -0300

    remove comments

--- examples/langchain-rag/langchain_rag/retriever.py
+++ examples/langchain-rag/langchain_rag/retriever.py
@@ -7,38 +7,29 @@ import faiss
 
 
 def create_retriever():
-    # Crear un modelo de embeddings
     embedding_model = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-    # Crear un FAISS index en memoria
     index = faiss.IndexFlatL2(1536)
 
-    # Crear un docstore en memoria
     docstore = InMemoryDocstore({})
 
-    # Crear un índice para mapear IDs de documentos
     index_to_docstore_id = {}
 
-    # Crear el vector store utilizando FAISS e InMemoryDocstore
     vector_store = FAISS(embedding_model,
                          index, docstore, index_to_docstore_id)
 
-    # Get the directory of the current file
     current_dir = os.path.dirname(__file__)
 
-    # Construct the full path to the target file
     public_doc_path = os.path.join(current_dir, '../assets/docs/public-doc.md')
     private_doc_path = os.path.join(
         current_dir, '../assets/docs/private-doc.md')
 
-    # Open and read the files
     with open(public_doc_path, 'r', encoding='utf-8') as file:
         public_doc_content = file.read()
 
     with open(private_doc_path, 'r', encoding='utf-8') as file:
         private_doc_content = file.read()
 
-    # Agregar documentos al vector store
     documents = [
         Document(page_content=public_doc_content,
                  metadata={"id": 1, "access": "public"}),
